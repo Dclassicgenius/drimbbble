95f09895a115077a69be450b58b9e49883f3c280
Dclassicgenius <patamaechi1@gmail.com>
2023-07-06 20:27:12 +0300

    footer and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section>
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>LoadMore</h1>
+    </section>
+  );
 };
 
 export default Home;
