2b92b6a7232d5819ecbe7b4473e9859797a71143
Dclassicgenius <patamaechi1@gmail.com>
2023-07-08 02:23:03 +0300

    user authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,19 +3,16 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
+import { signOut } from "next-auth/react";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
   return (
     <nav className="flexBetween navbar">
       <div className=" flex-1 flexStart gap-10">
         <Link href="/">
-          <Image
-            src="/drimblbble.png"
-            width={115}
-            height={43}
-            alt="Drimbbble"
-          />
+          <Image src="/drimbbble.png" width={115} height={43} alt="Drimbbble" />
         </Link>
 
         <ul className="xl:flex hidden text-small gap-7">
@@ -28,10 +25,23 @@ const Navbar = () => {
       </div>
 
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            {session.user?.image && (
+              <Link href={`/profile/${session?.user?.id}`}>
+                <Image
+                  src={session.user?.image}
+                  width={40}
+                  height={40}
+                  alt={session.user.name}
+                  className=" rounded-full"
+                />
+              </Link>
+            )}
             <Link href="/create-project">Share Work</Link>
+            <button type="button" className="text-sm">
+              Sign Out
+            </button>
           </>
         ) : (
           <AuthProviders />
