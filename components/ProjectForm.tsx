166af10622716be84bbedc81cdcc00f2eeb15285
Dclassicgenius <patamaechi1@gmail.com>
2023-07-11 16:55:49 +0300

    post creation and cloudinary for image upload

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -5,27 +5,68 @@ import FormField from "./FormField";
 import { categoryFilters } from "@/constants";
 import CustomMenu from "./CustomMenu";
 import { useState } from "react";
+import Button from "./Button";
+import { createNewProject, fetchToken } from "@/lib/actions";
+import { useRouter } from "next/navigation";
 
 type ProjectFormProps = {
   type: string;
   session: SessionInterface;
 };
 const ProjectForm = ({ type, session }: ProjectFormProps) => {
-  const [isSubmitting, setIssubmitting] = useState(false);
+  const router = useRouter();
+  const [submitting, setSubmitting] = useState(false);
   const [form, setForm] = useState({
     image: "",
     title: "",
     description: "",
-    liveSiteurl: "",
+    liveSiteUrl: "",
     githubUrl: "",
     category: "",
   });
 
-  const handleFormSubmit = (e: React.FormEvent) => {};
-  const handleChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {};
   const handleStateChange = (fieldName: string, value: string) => {
     setForm((prevState) => ({ ...prevState, [fieldName]: value }));
   };
+
+  const handleChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    if (!file.type.includes("image")) {
+      return alert("Please upload an image file");
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+
+      handleStateChange("image", result);
+    };
+  };
+
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setSubmitting(true);
+
+    const { token } = await fetchToken();
+
+    try {
+      if (type === "create") {
+        await createNewProject(form, session?.user?.id, token);
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
     <form onSubmit={handleFormSubmit} className="flexStart form">
       <div className="flexStart form_image-container">
@@ -37,9 +78,7 @@ const ProjectForm = ({ type, session }: ProjectFormProps) => {
           accept="image/*"
           required={type === "create"}
           type="file"
-          onChange={(e) => {
-            handleChangeImage;
-          }}
+          onChange={handleChangeImage}
           className="form_image-input"
         />
         {form.image && (
@@ -68,9 +107,9 @@ const ProjectForm = ({ type, session }: ProjectFormProps) => {
       <FormField
         type="url"
         title="Website URL"
-        state={form.liveSiteurl}
+        state={form.liveSiteUrl}
         placeholder="https://drimprofile.netlify.app/"
-        setState={(value) => handleStateChange("liveSiteurl", value)}
+        setState={(value) => handleStateChange("liveSiteUrl", value)}
       />
       <FormField
         type="url"
@@ -90,7 +129,16 @@ const ProjectForm = ({ type, session }: ProjectFormProps) => {
       />
 
       <div className="flexstart w-full">
-        <button>Create</button>
+        <Button
+          title={
+            submitting
+              ? `${type === "create" ? "Creating" : "Editing"}`
+              : `${type === "create" ? "Create" : "Edit"}`
+          }
+          type="submit"
+          leftIcon={submitting ? "" : "/plus.svg"}
+          submitting={submitting}
+        />
       </div>
     </form>
   );
