db946ddc314dbb7942bef7fb1e398a4fea609303
Dclassicgenius <patamaechi1@gmail.com>
2023-07-14 13:23:29 +0300

    fix client error issue on deploy

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export const revalidate = 0;
 
 const Home = async ({ searchParams: { category, endcursor } }: Props) => {
   const data = (await fetchAllProjects(
-    category || null,
+    category || "",
     endcursor || null
   )) as ProjectSearch;
 
